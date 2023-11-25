9ccd7dc62a745b182ed99ccd402b19782c6627d3
Harshr24 <137977796+Harshr24@users.noreply.github.com>
2023-11-25 15:55:59 +0000

    Sat, Nov 25, 2023, 9:25 PM +05:30

--- packages/log.sql
+++ packages/log.sql
@@ -1,6 +1,6 @@
 
 -- *** The Lost Letter ***
-
+SELECT * FROM addresses WHERE id IN (SELECT address_id FROM scans WHERE package_id IN (SELECT id FROM packages WHERE from_address_id = (SELECT id FROM addresses WHERE address = '900 Somerville Avenue' ) AND to_address_id = (SELECT id FROM addresses WHERE address LIKE '2 Fin%' )));
 -- *** The Devious Delivery ***
 
 -- *** The Forgotten Gift ***
