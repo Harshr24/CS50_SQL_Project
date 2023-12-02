c15612d816d2418b3e03562f47aaf682d3274fd4
Harshr24 <137977796+Harshr24@users.noreply.github.com>
2023-12-02 18:11:49 +0000

    Sat, Dec 2, 2023, 11:41 PM +05:30

--- moneyball/3.sql
+++ moneyball/3.sql
@@ -1,1 +1,1 @@
-SELECT year, HR AS "home runs" FROM performances WHERE player_id = (SELECT id FROM players WHERE first_name = 'Ken' AND last_name = 'Griffey') ORDER BY year DESC;
+SELECT year, HR FROM performances WHERE player_id = (SELECT id FROM players WHERE first_name = 'Ken' AND last_name = 'Griffey') ORDER BY year DESC;
