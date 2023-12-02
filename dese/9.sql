249bf152e576f3cb21b13a73446fe16f31196660
Harshr24 <137977796+Harshr24@users.noreply.github.com>
2023-12-02 17:32:36 +0000

    Sat, Dec 2, 2023, 11:02 PM +05:30

--- dese/9.sql
+++ dese/9.sql
@@ -1,1 +1,1 @@
-SELECT d.name,e.pupils FROM districts d JOIN expenditures e ON e.district_id = d.id ORDER BY pupils ASC limit 1;
+SELECT d.name FROM districts d JOIN expenditures e ON e.district_id = d.id ORDER BY pupils ASC limit 1;
