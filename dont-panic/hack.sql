e427cdcb3ad6457a8a83389c1639bb815f54879f
Harshr24 <137977796+Harshr24@users.noreply.github.com>
2023-12-05 05:42:28 +0000

    Tue, Dec 5, 2023, 11:12 AM +05:30

--- dont-panic/hack.sql
+++ dont-panic/hack.sql
@@ -6,4 +6,4 @@ DELETE FROM "user_logs" WHERE "type" = 'update' AND "old_username" = 'admin';
 
 -- Add false data to throw others off your trail - frame emily33 for password change to admin's password
 INSERT INTO "user_logs" ("type", "old_username", "new_username", "old_password", "new_password")
-SELECT 'update', 'admin','admin', NULL, (SELECT password FROM usres WHERE username ='emily33');
+SELECT 'update', 'admin','admin', NULL, (SELECT password FROM users WHERE username ='emily33');
