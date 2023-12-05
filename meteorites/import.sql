5678a48d4350a273d3cfff92424d6a96151e0605
Harshr24 <137977796+Harshr24@users.noreply.github.com>
2023-12-05 06:08:57 +0000

    Tue, Dec 5, 2023, 11:38 AM +05:30

--- meteorites/import.sql
+++ meteorites/import.sql
@@ -6,4 +6,6 @@ UPDATE meteorites_temp SET lat = NULL WHERE lat = 0;
 UPDATE meteorites_temp SET long = NULL WHERE long = 0;
 UPDATE meteorites_temp SET year = NULL WHERE year = 0 OR year LIKE '';
 
-UPDATE meteorites_temp SET year = NULL WHERE year = 0;
+UPDATE meteorites_temp SET mass = ROUND(mass,2), lat= ROUND(lat,2), long = ROUND(long,2);
+
+CREATE TABLE meteorites(name TEXT, id INTEGER, class TEXT, mass REAL, discovery TEXT, year INTEGER, lat REAL, long REAL, PRIMARY KEY ('id));
