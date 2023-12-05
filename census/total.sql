422683d6af2b78d563b778724001e3137bacfd59
Harshr24 <137977796+Harshr24@users.noreply.github.com>
2023-12-05 11:42:21 +0000

    Tue, Dec 5, 2023, 5:12 PM +05:30

---
+++ census/total.sql
@@ -0,0 +1,9 @@
+CREATE VIEW total AS
+SELECT
+ 'Total' AS district,
+ SUM(families) AS families,
+ SUM(households) AS households,
+ SUM(population) AS population,
+ SUM(male) AS male,
+ SUM(female) AS female
+FROM census;
