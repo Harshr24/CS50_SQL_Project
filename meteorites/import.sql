914acff24c6054ce2e65fa1684363a238a6fc6f4
Harshr24 <137977796+Harshr24@users.noreply.github.com>
2023-12-05 06:25:05 +0000

    Tue, Dec 5, 2023, 11:55 AM +05:30

--- meteorites/import.sql
+++ meteorites/import.sql
@@ -9,7 +9,7 @@ UPDATE meteorites_temp SET long = NULL WHERE long = 0;
 UPDATE meteorites_temp SET year = NULL WHERE year = 0 OR year IS NULL OR year = '';
 UPDATE meteorites_temp SET mass = ROUND(mass, 2), lat = ROUND(lat, 2), long = ROUND(long, 2);
 
-CREATE TABLE meteorites (name TEXT, id INTEGER PRIMARY KEY, class TEXT, mass REAL, discovery TEXT, year INTEGER, lat REAL, long REAL);
+CREATE TABLE meteorites (name TEXT, id INTEGER, class TEXT, mass REAL, discovery TEXT, year INTEGER, lat REAL, long REAL, PRIMARY KEY('id'));
 INSERT INTO meteorites (name, id, class, mass, discovery, year, lat, long) SELECT name, id, class, mass, discovery, year, lat, long FROM meteorites_temp WHERE nametype NOT LIKE '%Relict%' ORDER BY year, name COLLATE NOCASE;
 
 DROP TABLE IF EXISTS meteorites_temp;
