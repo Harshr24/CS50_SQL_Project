31902329ecd98a715251fddcdfc76bad5e6cbbda
Harshr24 <137977796+Harshr24@users.noreply.github.com>
2023-12-05 06:06:15 +0000

    Tue, Dec 5, 2023, 11:36 AM +05:30

--- meteorites/import.sql
+++ meteorites/import.sql
@@ -6,3 +6,4 @@ UPDATE meteorites_temp SET lat = NULL WHERE lat = 0;
 UPDATE meteorites_temp SET long = NULL WHERE long = 0;
 UPDATE meteorites_temp SET year = NULL WHERE year = 0 OR year LIKE '';
 
+UPDATE meteorites_temp SET year = NULL WHERE year = 0;
