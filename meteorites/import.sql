20ec57d9d5a1017f3ad21a616338ec2cbaec9bf4
Harshr24 <137977796+Harshr24@users.noreply.github.com>
2023-12-05 06:02:29 +0000

    Tue, Dec 5, 2023, 11:32 AM +05:30

--- meteorites/import.sql
+++ meteorites/import.sql
@@ -1,2 +1,5 @@
 CREATE TABLE meteorites_temp(name TEXT, id INTEGER, nametype TEXT, class TEXT, mass REAL, discovery TEXT, year INTEGER, lat REAL, long REAL, PRIMARY KEY ('id));
 .import --csv --skip 1 meteorites.csv meteorites_temp
+
+UPDATE meteorites_temp SET mass = NULL WHERE mass = 0;
+UPDATE meteorites_temp SET lat = NULL WHERE lat = 0;
