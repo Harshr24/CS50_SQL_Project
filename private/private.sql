66bee0fee1a40b31c0edd5d85b23c0c88d8b9d89
Harshr24 <137977796+Harshr24@users.noreply.github.com>
2023-12-06 03:31:57 +0000

    Wed, Dec 6, 2023, 9:01 AM +05:30

--- private/private.sql
+++ private/private.sql
@@ -1,21 +1,21 @@
-DROP VIEW IF EXISTS "message";
-CREATE VIEW "message" AS
-SELECT trim(substr(sentence, 2, 4)) AS phrase FROM sentences WHERE id = 14
-UNION SELECT trim(substr(sentence, 98, 3)) AS phrase FROM sentences WHERE id = 98
-UNION SELECT trim(substr(sentence, 4, 5)) AS phrase FROM sentences WHERE id = 4
-UNION SELECT trim(substr(sentence, 114, 3)) AS phrase FROM sentences WHERE id = 114
-UNION SELECT trim(substr(sentence, 3, 5)) AS phrase FROM sentences WHERE id = 3
-UNION SELECT trim(substr(sentence, 5, 4)) AS phrase FROM sentences WHERE id = 5
-UNION SELECT trim(substr(sentence, 618, 9)) AS phrase FROM sentences WHERE id = 618
-UNION SELECT trim(substr(sentence, 72, 7)) AS phrase FROM sentences WHERE id = 72
-UNION SELECT trim(substr(sentence, 9, 3)) AS phrase FROM sentences WHERE id = 9
-UNION SELECT trim(substr(sentence, 630, 7)) AS phrase FROM sentences WHERE id = 630
-UNION SELECT trim(substr(sentence, 7, 3)) AS phrase FROM sentences WHERE id = 7
-UNION SELECT trim(substr(sentence, 932, 12)) AS phrase FROM sentences WHERE id = 932
-UNION SELECT trim(substr(sentence, 12, 5)) AS phrase FROM sentences WHERE id = 12
-UNION SELECT trim(substr(sentence, 2230, 50)) AS phrase FROM sentences WHERE id = 2230
-UNION SELECT trim(substr(sentence, 50, 7)) AS phrase FROM sentences WHERE id = 50
-UNION SELECT trim(substr(sentence, 2346, 44)) AS phrase FROM sentences WHERE id = 2346
-UNION SELECT trim(substr(sentence, 44, 10)) AS phrase FROM sentences WHERE id = 44
-UNION SELECT trim(substr(sentence, 3041, 14)) AS phrase FROM sentences WHERE id = 3041
-UNION SELECT trim(substr(sentence, 14, 5)) AS phrase FROM sentences WHERE id = 14;
+CREATE VIEW message AS
+SELECT sentence FROM sentences
+WHERE id IN (14, 114, 618, 630, 932, 2230, 2346, 3041);
+
+CREATE TABLE temp_sentences (
+    id INTEGER,
+    sentence TEXT
+);
+
+INSERT INTO temp_sentences VALUES
+(14, 'Quite so! You have not observed.'),
+(114, 'And yet you have seen.');
+-- and so on for the other triplets
+
+CREATE VIEW message AS
+SELECT
+    substr(sentence,
+        CASE id WHEN 14 THEN 98 WHEN 114 THEN 3 END,
+        CASE id WHEN 14 THEN 4 WHEN 114 THEN 5 END) AS phrase
+FROM temp_sentences
+WHERE id IN (14, 114, 618, 630, 932, 2230, 2346, 3041);
