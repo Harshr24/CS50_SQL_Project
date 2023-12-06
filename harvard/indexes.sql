3c812a326dcf99fa1bc26d97679fdae92eb6ed8d
Harshr24 <137977796+Harshr24@users.noreply.github.com>
2023-12-06 08:55:35 +0000

    Wed, Dec 6, 2023, 2:25 PM +05:30

--- harvard/indexes.sql
+++ harvard/indexes.sql
@@ -1,20 +1,14 @@
 -- Index on enrollments table for student_id column
 CREATE INDEX idx_enrollments_student_id ON enrollments(student_id);
-
 -- Index on enrollments table for course_id column
 CREATE INDEX idx_enrollments_course_id ON enrollments(course_id);
-
 -- Index on courses table for department, number, and semester columns
 CREATE INDEX idx_courses_dept_num_semester ON courses(department, number, semester);
-
 -- Index on courses table for title column
 CREATE INDEX idx_courses_title ON courses(title);
-
 -- Index on requirements table for name column
 CREATE INDEX idx_requirements_name ON requirements(name);
-
 -- Index on satisfies table for course_id column
 CREATE INDEX idx_satisfies_course_id ON satisfies(course_id);
-
 -- Index on satisfies table for requirement_id column
 CREATE INDEX idx_satisfies_requirement_id ON satisfies(requirement_id);
