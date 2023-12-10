052a93f92214ae27a6e971a73775dcca075f2731
Harshr24 <137977796+Harshr24@users.noreply.github.com>
2023-12-10 11:29:47 +0000

    Sun, Dec 10, 2023, 4:59 PM +05:30

--- project/schema.sql
+++ project/schema.sql
@@ -6,33 +6,13 @@ CREATE TABLE patients (patient_id INTEGER PRIMARY KEY, name TEXT NOT NULL, age I
 CREATE TABLE doctors (doctor_id INTEGER PRIMARY KEY, name TEXT NOT NULL, specialization TEXT, contact_number TEXT);
 
 -- Table: appointments
-CREATE TABLE appointments (
-    appointment_id INTEGER PRIMARY KEY,
-    patient_id INTEGER NOT NULL,
-    doctor_id INTEGER NOT NULL,
-    appointment_date TIMESTAMP,
-    purpose TEXT,
-    FOREIGN KEY(patient_id) REFERENCES patients(patient_id),
-    FOREIGN KEY(doctor_id) REFERENCES doctors(doctor_id)
-);
+CREATE TABLE appointments (appointment_id INTEGER PRIMARY KEY, patient_id INTEGER NOT NULL, doctor_id INTEGER NOT NULL, appointment_date TIMESTAMP, purpose TEXT, FOREIGN KEY(patient_id) REFERENCES patients(patient_id), FOREIGN KEY(doctor_id) REFERENCES doctors(doctor_id));
 
 -- Table: medical_records
-CREATE TABLE medical_records (
-    record_id INTEGER PRIMARY KEY,
-    patient_id INTEGER NOT NULL,
-    doctor_id INTEGER NOT NULL,
-    diagnosis TEXT,
-    prescription TEXT,
-    FOREIGN KEY(patient_id) REFERENCES patients(patient_id),
-    FOREIGN KEY(doctor_id) REFERENCES doctors(doctor_id)
-);
+CREATE TABLE medical_records (record_id INTEGER PRIMARY KEY, patient_id INTEGER NOT NULL, doctor_id INTEGER NOT NULL, diagnosis TEXT, prescription TEXT, FOREIGN KEY(patient_id) REFERENCES patients(patient_id), FOREIGN KEY(doctor_id) REFERENCES doctors(doctor_id));
 
 -- Table: departments
-CREATE TABLE departments (
-    department_id INTEGER PRIMARY KEY,
-    name TEXT NOT NULL,
-    location TEXT
-);
+CREATE TABLE departments (department_id INTEGER PRIMARY KEY, name TEXT NOT NULL, location TEXT);
 
 -- Create an index to speed up searches on the patients' names
 CREATE INDEX idx_patients_name ON patients (name);
@@ -41,8 +21,4 @@ CREATE INDEX idx_patients_name ON patients (name);
 CREATE INDEX idx_doctors_specialization ON doctors (specialization);
 
 -- Create a view to display patient appointments with associated doctors' details
-CREATE VIEW patient_appointments AS
-SELECT a.appointment_id, p.name AS patient_name, d.name AS doctor_name, a.appointment_date
-FROM appointments a
-JOIN patients p ON a.patient_id = p.patient_id
-JOIN doctors d ON a.doctor_id = d.doctor_id;
+CREATE VIEW patient_appointments AS SELECT a.appointment_id, p.name AS patient_name, d.name AS doctor_name, a.appointment_date FROM appointments a JOIN patients p ON a.patient_id = p.patient_id JOIN doctors d ON a.doctor_id = d.doctor_id;
