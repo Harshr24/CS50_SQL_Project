16baf1dc638c5ddcdd84d136525dbd6b430f04fc
Harshr24 <137977796+Harshr24@users.noreply.github.com>
2023-12-10 06:25:20 +0000

    Sun, Dec 10, 2023, 11:55 AM +05:30

--- project/schema.sql
+++ project/schema.sql
@@ -1,1 +1,46 @@
 -- In this SQL file, write (and comment!) the schema of your database, including the CREATE TABLE, CREATE INDEX, CREATE VIEW, etc. statements that compose it
+-- Table: patients
+CREATE TABLE patients (
+    patient_id INTEGER PRIMARY KEY,
+    name TEXT NOT NULL,
+    age INTEGER,
+    gender TEXT,
+    contact_number TEXT
+);
+
+-- Table: doctors
+CREATE TABLE doctors (
+    doctor_id INTEGER PRIMARY KEY,
+    name TEXT NOT NULL,
+    specialization TEXT,
+    contact_number TEXT
+);
+
+-- Table: appointments
+CREATE TABLE appointments (
+    appointment_id INTEGER PRIMARY KEY,
+    patient_id INTEGER NOT NULL,
+    doctor_id INTEGER NOT NULL,
+    appointment_date TIMESTAMP,
+    purpose TEXT,
+    FOREIGN KEY(patient_id) REFERENCES patients(patient_id),
+    FOREIGN KEY(doctor_id) REFERENCES doctors(doctor_id)
+);
+
+-- Table: medical_records
+CREATE TABLE medical_records (
+    record_id INTEGER PRIMARY KEY,
+    patient_id INTEGER NOT NULL,
+    doctor_id INTEGER NOT NULL,
+    diagnosis TEXT,
+    prescription TEXT,
+    FOREIGN KEY(patient_id) REFERENCES patients(patient_id),
+    FOREIGN KEY(doctor_id) REFERENCES doctors(doctor_id)
+);
+
+-- Table: departments
+CREATE TABLE departments (
+    department_id INTEGER PRIMARY KEY,
+    name TEXT NOT NULL,
+    location TEXT
+);
